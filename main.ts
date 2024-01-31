7f88eb710e247d1fff131da0fc5c165c9909b24d
Yony Calsin <helloyonycalsin@gmail.com>
2024-01-31 13:03:43 -0500

    feat: generate the basic report

---
+++ main.ts
@@ -0,0 +1,31 @@
+import path from "node:path";
+import { promisify } from "node:util";
+import { exec } from "node:child_process";
+import sanitizeFilename from "sanitize-filename";
+
+const execAsync = promisify(exec);
+
+const OUTPUT_FOLDER = path.join(process.cwd(), "reports");
+
+const URLS = ["https://www.google.com/"];
+
+async function main() {
+  for await (const url of URLS) {
+    const outputFilename = sanitizeFilename(url) + ".html";
+
+    const outputPath = path.join(OUTPUT_FOLDER, outputFilename);
+
+    const command = `npx lighthouse "${url}" --output html --output-path "${outputPath}"`;
+
+    try {
+      const result = await execAsync(command);
+
+      console.log("stderr:", result.stderr || "not defined");
+      console.log("stdout:", result.stdout);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+main();
